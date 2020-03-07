a82aa29a526a6a8021ed38e1f5df8fbff35cd485
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-03-07 21:57:51 +0200

    Migrations

--- migrations/m200307_214824_live_search.php
+++ migrations/m200307_214824_live_search.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m200202_223527_live_search
+ * Class m200307_214824_live_search
  */
-class m200202_223527_live_search extends Migration
+class m200307_214824_live_search extends Migration
 {
     /**
      * {@inheritdoc}
@@ -33,6 +33,16 @@ class m200202_223527_live_search extends Migration
             )");
         }
 
+        if (class_exists('\wdmg\blog\models\Posts')) {
+            $tableName = \wdmg\blog\models\Posts::tableName();
+            $this->execute("ALTER TABLE $tableName ADD FULLTEXT INDEX {{%idx-blog-search}} (
+              `title`,
+              `description`,
+              `keywords`,
+              `content`
+            )");
+        }
+
     }
 
     /**
@@ -50,6 +60,11 @@ class m200202_223527_live_search extends Migration
         if (class_exists('\wdmg\news\models\News')) {
             $tableName = \wdmg\news\models\News::tableName();
             $this->dropIndex('{{%idx-news-search}}', $tableName);
+        }
+
+        if (class_exists('\wdmg\blog\models\Posts')) {
+            $tableName = \wdmg\blog\models\Posts::tableName();
+            $this->dropIndex('{{%idx-blog-search}}', $tableName);
         }
 
     }
